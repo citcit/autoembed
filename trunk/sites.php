d31383fb440a22c8e2851f5aa6112792a39fc3da
jhines <jhines@e2e31596-d759-11dd-a5e1-83afafa30df3>
2008-12-31 19:17:32 +0000

    license

--- trunk/sites.php
+++ trunk/sites.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * This file is part of AutoEmbed.
+ *
+ * AutoEmbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AutoEmbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AutoEmbed.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* Explanation of items
 	Mandatory ones must be specified, however if it defaults to something, it won't cause a problem if its left off. Optional ones aren't needed.
 	All [regex] patterns are only partial. So don't specify delimiters etc.  The regex is completed when its used.
