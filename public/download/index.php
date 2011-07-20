152869918001c5dd8b1f831a44e8505a93133802
jhines <jhines@gmail.com>
2011-07-20 14:53:12 +0000

    updating version to 1.8

--- public/download/index.php
+++ public/download/index.php
@@ -15,8 +15,7 @@ and <em>Jason Hines</em> of <a target="_new" href="http://devtwo.com/">DevTwo So
 <p>AutoEmbed is licensed under the <a href="http://www.gnu.org/licenses/lgpl.txt">GNU Lesser General Public License</a> and free to use for non-commercial use only.
 If you intend to use AutoEmbed in your commercial project, you must purchase a license below.</p>
 
-<p><img src="<?=BASE_URL?>/images/download.png" align="absmiddle" /> <a href="http://autoembed.googlecode.com/files/AutoEmbed-1.6.tar.gz">Download AutoEmbed</a> (Version 1.6 Released 2/23/10)</p>
-
+<p><img src="<?=BASE_URL?>/images/download.png" align="absmiddle" /> <a href="http://autoembed.googlecode.com/files/AutoEmbed-1.8.tar.gz">Download AutoEmbed</a> (Version 1.8 Released 7/20/11)</p>
 
 <div class="box">
   <div style="float:left;width:50%;">
